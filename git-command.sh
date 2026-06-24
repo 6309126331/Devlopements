f10c762dc0b461ad0f00107343f41163e3a3d741
Sibbala <narasimha1719@gmail.com>
2026-06-24 09:52:09 +0530

    massage

---
+++ git-command.sh
@@ -0,0 +1,11 @@
+git init
+git add .
+git commit -m "massage"
+git branch -M main
+git remote add origin githuburl
+git push -u origin main
+
+to push the code from second time
+git add .
+git commit -m "massage"
+git push
